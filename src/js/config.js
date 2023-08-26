c393f9c376caa906275663403863da285799c88c
Rehtest <rehtesten@gmail.com>
2023-08-26 11:55:07 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a909dd11-b100-4feb-a6fd-1d73b8594782';
+export const MODAL_CLOSE_SEC = 2.5;
